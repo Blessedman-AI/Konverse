bc7822eb70164c116247a2247101a8050ea07d03
Blessedman-AI <beemhanalabo@gmail.com>
2024-03-01 15:23:15 +0100

    Fixing images not showing on live site

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -4,8 +4,6 @@ import Link from 'next/link';
 import ThemeToggle from '../themeToggle/ThemeToggle';
 import AuthLinks from '../authLinks/AuthLinks';
 
-
-
 const Navbar = () => {
   return (
     <div className={styles.container}>
@@ -17,7 +15,7 @@ const Navbar = () => {
           height={24}
         />
         <Image
-          src="/images/instagram.png"
+          src={'/images/instagram.png'}
           alt="instagram icon"
           width={24}
           height={24}
@@ -38,9 +36,15 @@ const Navbar = () => {
       <div className={styles.logo}>MyBlog</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/" className={styles.link}>Homepage</Link>
-        <Link href="/" className={styles.link}>Contact</Link>
-        <Link href="/" className={styles.link}>About</Link>
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
